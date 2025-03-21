3ee85a23e1b06ebccac214596f4dee157d2209ac
prajapatankit8875 <ankitkumawat027@gmail.com>
2025-03-21 12:05:48 +0530

    Data Insertion and Querying

--- flight/models.py
+++ flight/models.py
@@ -14,5 +14,44 @@ class airport(models.Model):
     def __str__(self):
         return self.airport_name 
     
+class flight(models.Model): 
+    flight_number = models.CharField(max_length=10)
+    departure_airport = models.ForeignKey(airport, on_delete=models.CASCADE)
+    arrival_airport = models.ForeignKey(airport, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField() 
+    price = models.FloatField()
+    duration = models.FloatField()
+    status = models.CharField(max_length=15, default='Scheduled', choices=[('Scheduled', 'Scheduled'), ('Delayed', 'Delayed'), ('Cancelled', 'Cancelled')])
+    delay_time = models.FloatField()
+    
+    def __str__(self):
+        return self.flight_number
+    
+class airline(models.Model):
+    airline_name = models.CharField(max_length=100)
+    iata_code = models.CharField(max_length=10)
+    icao_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=15)
+    logo = models.ImageField(upload_to='airline_logo')
+    
+    def __str__(self):
+        return self.airline_name
+    
+class passenger(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    passport_number = models.CharField
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)          
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=15)
+    country = models.CharField(max_length=15)
+    postal_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+    
+    
     
 
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from .models import airport, airline, flight, passenger
 # Create your views here.
+class AirportListView(ListView):
+    model = airport
+    template_name = 'flight/airport_list.html'
+    context_object_name = 'airport_list'  
+
+class FlightListView(ListView):
+    model = flight
+    template_name = 'flight/flight_list.html'
+    context_object_name = 'flight_list'
+
+class AirlineListView(ListView):
+    model = airline
+    template_name = 'flight/airline_list.html'
+    context_object_name = 'airline_list'
+    
+
+class PassengerListView(ListView):      
+    model = passenger
+    template_name = 'flight/passenger_list.html'
+    context_object_name = 'passenger_list'
+    return render(request, 'flight/passenger_list.html', {'passenger_list': passenger_list})      
+
